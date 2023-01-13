48ae30a58a26f0430339233acb788ba5ff5b3c99
Valentina Moretti <valentina.moretti@mail.polimi.it>
2023-01-13 22:26:08 +0100

    mesh

--- src/Prion.cpp
+++ src/Prion.cpp
@@ -13,7 +13,7 @@ HeatNonLinear::setup()
     grid_in.attach_triangulation(mesh_serial);
 
     const std::string mesh_file_name =
-      "../mesh/mesh-cube-5.msh";
+      "../mesh/brain-simple-mesh.fbx";
 
     std::ifstream grid_in_file(mesh_file_name);
     grid_in.read_msh(grid_in_file);
