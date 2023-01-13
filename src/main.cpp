630bdbcfb8dcd937500aa7de7f4fe83f8051f887
Valentina Moretti <valentina.moretti@mail.polimi.it>
2023-01-13 22:19:10 +0100

    meglio

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include "Prion.hpp"
+
+// Main function.
+int
+main(int argc, char *argv[])
+{
+  Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
+
+  const unsigned int N      = 200;
+  const unsigned int degree = 1;
+
+  const double T      = 20.0;
+  const double deltat = 0.1;
+
+  HeatNonLinear problem(N, degree, T, deltat);
+
+  problem.setup();
+  problem.solve();
+
+  return 0;
+}
