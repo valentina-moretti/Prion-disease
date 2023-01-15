c494ac351e882707a8291c8e93bb792e70cfe656
Roberto Neglia <roberto.neglia@mail.polimi.it>
2023-01-15 10:47:40 +0100

    gitignore fix + simple brain mesh

--- src/Prion.cpp
+++ src/Prion.cpp
@@ -1,7 +1,6 @@
 #include "Prion.hpp"
 
-void
-HeatNonLinear::setup()
+void HeatNonLinear::setup()
 {
   // Create the mesh.
   {
@@ -13,14 +12,14 @@ HeatNonLinear::setup()
     grid_in.attach_triangulation(mesh_serial);
 
     const std::string mesh_file_name =
-      "../mesh/brain-simple-mesh.fbx";
+        "../mesh/brain-simple-mesh.msh";
 
     std::ifstream grid_in_file(mesh_file_name);
     grid_in.read_msh(grid_in_file);
 
     GridTools::partition_triangulation(mpi_size, mesh_serial);
     const auto construction_data = TriangulationDescription::Utilities::
-      create_description_from_triangulation(mesh_serial, MPI_COMM_WORLD);
+        create_description_from_triangulation(mesh_serial, MPI_COMM_WORLD);
     mesh.create_triangulation(construction_data);
 
     pcout << "  Number of elements = " << mesh.n_global_active_cells()
@@ -87,19 +86,18 @@ HeatNonLinear::setup()
   }
 }
 
-void
-HeatNonLinear::assemble_system()
+void HeatNonLinear::assemble_system()
 {
   const unsigned int dofs_per_cell = fe->dofs_per_cell;
-  const unsigned int n_q           = quadrature->size();
+  const unsigned int n_q = quadrature->size();
 
   FEValues<dim> fe_values(*fe,
                           *quadrature,
                           update_values | update_gradients |
-                            update_quadrature_points | update_JxW_values);
+                              update_quadrature_points | update_JxW_values);
 
   FullMatrix<double> cell_matrix(dofs_per_cell, dofs_per_cell);
-  Vector<double>     cell_residual(dofs_per_cell);
+  Vector<double> cell_residual(dofs_per_cell);
 
   std::vector<types::global_dof_index> dof_indices(dofs_per_cell);
 
@@ -107,90 +105,82 @@ HeatNonLinear::assemble_system()
   residual_vector = 0.0;
 
   // Value and gradient of the solution on current cell.
-  std::vector<double>         solution_loc(n_q);
+  std::vector<double> solution_loc(n_q);
   std::vector<Tensor<1, dim>> solution_gradient_loc(n_q);
 
   // Value of the solution at previous timestep (un) on current cell.
   std::vector<double> solution_old_loc(n_q);
 
   for (const auto &cell : dof_handler.active_cell_iterators())
+  {
+    if (!cell->is_locally_owned())
+      continue;
+
+    fe_values.reinit(cell);
+
+    cell_matrix = 0.0;
+    cell_residual = 0.0;
+
+    fe_values.get_function_values(solution, solution_loc);             // u n+1
+    fe_values.get_function_gradients(solution, solution_gradient_loc); // grad u n+1
+    fe_values.get_function_values(solution_old, solution_old_loc);     // u n
+
+    for (unsigned int q = 0; q < n_q; ++q)
     {
-      if (!cell->is_locally_owned())
-        continue;
+      // Evaluate coefficients on this quadrature node.
+      const double alpha_loc = alpha.value(fe_values.quadrature_point(q));
+      const double spreading_loc = spreading.value(fe_values.quadrature_point(q));
 
-      fe_values.reinit(cell);
-
-      cell_matrix   = 0.0;
-      cell_residual = 0.0;
-
-      fe_values.get_function_values(solution, solution_loc);              //u n+1
-      fe_values.get_function_gradients(solution, solution_gradient_loc);  //grad u n+1
-      fe_values.get_function_values(solution_old, solution_old_loc);      //u n
-
-      for (unsigned int q = 0; q < n_q; ++q)
+      for (unsigned int i = 0; i < dofs_per_cell; ++i)
+      {
+        for (unsigned int j = 0; j < dofs_per_cell; ++j)
         {
-          // Evaluate coefficients on this quadrature node.
-          const double alpha_loc = alpha.value(fe_values.quadrature_point(q));
-          const double spreading_loc = spreading.value(fe_values.quadrature_point(q));
 
-          for (unsigned int i = 0; i < dofs_per_cell; ++i)
-            {
-              for (unsigned int j = 0; j < dofs_per_cell; ++j)
-                {
-                  
-                  // ------------------------------------------- (A.1) ------------------------------------------- //
-                  // Mass matrix.
-                  cell_matrix(i, j) += fe_values.shape_value(i, q) *
-                                       fe_values.shape_value(j, q) / deltat *
-                                       fe_values.JxW(q);
+          // ------------------------------------------- (A.1) ------------------------------------------- //
+          // Mass matrix.
+          cell_matrix(i, j) += fe_values.shape_value(i, q) *
+                               fe_values.shape_value(j, q) / deltat *
+                               fe_values.JxW(q);
 
-                  
-                  // ------------------------------------------- (A.2) ------------------------------------------- //
-                  // Non-linear stiffness matrix, first term.
-                  cell_matrix(i, j) += fe_values.shape_grad(i, q) * spreading_loc *
-                                       fe_values.shape_grad(j, q) *
-                                       fe_values.JxW(q);
-                
-                  // ------------------------------------------- (A.3) ------------------------------------------- // 
-                  // Non-linear stiffness matrix, second term.
-                  cell_matrix(i, j) -=  fe_values.shape_value(i, q) * alpha_loc *
-                                        ( 1 - 2 * solution_loc[q] ) * 
-                                        fe_values.shape_value(j, q) * fe_values.JxW(q);
-                    
-                  
+          // ------------------------------------------- (A.2) ------------------------------------------- //
+          // Non-linear stiffness matrix, first term.
+          cell_matrix(i, j) += fe_values.shape_grad(i, q) * spreading_loc *
+                               fe_values.shape_grad(j, q) *
+                               fe_values.JxW(q);
 
-                }
-
-              // Assemble the residual vector (with changed sign).
-
-              // ------------------------------------------- (R.1) ------------------------------------------- //
-              // Time derivative term.
-              cell_residual(i) += (solution_loc[q] - solution_old_loc[q]) /
-                                  deltat * fe_values.shape_value(i, q) *
-                                  fe_values.JxW(q);
-
-
-              // ------------------------------------------- (R.2) ------------------------------------------- //
-              cell_residual(i) -= fe_values.shape_grad(i, q) * spreading_loc *
-                                  solution_gradient_loc[q] *
-                                  fe_values.JxW(q);
-
-
-              // ------------------------------------------- (R.3) ------------------------------------------- //
-              // Diffusion term.
-              cell_residual(i) -= (alpha_loc * solution_loc[q] * ( 1 - solution_loc[q]) ) *
-                                   fe_values.shape_value(i, q) *
-                                   fe_values.JxW(q);
-
-
-            }
+          // ------------------------------------------- (A.3) ------------------------------------------- //
+          // Non-linear stiffness matrix, second term.
+          cell_matrix(i, j) -= fe_values.shape_value(i, q) * alpha_loc *
+                               (1 - 2 * solution_loc[q]) *
+                               fe_values.shape_value(j, q) * fe_values.JxW(q);
         }
 
-      cell->get_dof_indices(dof_indices);
+        // Assemble the residual vector (with changed sign).
 
-      jacobian_matrix.add(dof_indices, cell_matrix);
-      residual_vector.add(dof_indices, cell_residual);
+        // ------------------------------------------- (R.1) ------------------------------------------- //
+        // Time derivative term.
+        cell_residual(i) += (solution_loc[q] - solution_old_loc[q]) /
+                            deltat * fe_values.shape_value(i, q) *
+                            fe_values.JxW(q);
+
+        // ------------------------------------------- (R.2) ------------------------------------------- //
+        cell_residual(i) -= fe_values.shape_grad(i, q) * spreading_loc *
+                            solution_gradient_loc[q] *
+                            fe_values.JxW(q);
+
+        // ------------------------------------------- (R.3) ------------------------------------------- //
+        // Diffusion term.
+        cell_residual(i) -= (alpha_loc * solution_loc[q] * (1 - solution_loc[q])) *
+                            fe_values.shape_value(i, q) *
+                            fe_values.JxW(q);
+      }
     }
+
+    cell->get_dof_indices(dof_indices);
+
+    jacobian_matrix.add(dof_indices, cell_matrix);
+    residual_vector.add(dof_indices, cell_residual);
+  }
 
   jacobian_matrix.compress(VectorOperation::add);
   residual_vector.compress(VectorOperation::add);
@@ -204,7 +194,7 @@ HeatNonLinear::assemble_system()
     std::map<types::global_dof_index, double> boundary_values;
 
     std::map<types::boundary_id, const Function<dim> *> boundary_functions;
-    Functions::ZeroFunction<dim>                        zero_function;
+    Functions::ZeroFunction<dim> zero_function;
 
     for (unsigned int i = 0; i < 6; ++i)
       boundary_functions[i] = &zero_function;
@@ -214,34 +204,31 @@ HeatNonLinear::assemble_system()
                                              boundary_values);
 
     MatrixTools::apply_boundary_values(
-      boundary_values, jacobian_matrix, delta_owned, residual_vector, false);
+        boundary_values, jacobian_matrix, delta_owned, residual_vector, false);
   }
 }
 
-
 // TODO CHOOSE THE BETTER PRECONDITIONER
-void
-HeatNonLinear::solve_linear_system()
+void HeatNonLinear::solve_linear_system()
 {
   SolverControl solver_control(1000, 1e-6 * residual_vector.l2_norm());
 
   SolverCG<TrilinosWrappers::MPI::Vector> solver(solver_control);
-  TrilinosWrappers::PreconditionSSOR      preconditioner;
+  TrilinosWrappers::PreconditionSSOR preconditioner;
   preconditioner.initialize(
-    jacobian_matrix, TrilinosWrappers::PreconditionSSOR::AdditionalData(1.0));
+      jacobian_matrix, TrilinosWrappers::PreconditionSSOR::AdditionalData(1.0));
 
   solver.solve(jacobian_matrix, delta_owned, residual_vector, preconditioner);
   pcout << "  " << solver_control.last_step() << " CG iterations" << std::endl;
 }
 
-void
-HeatNonLinear::solve_newton()
+void HeatNonLinear::solve_newton()
 {
-  const unsigned int n_max_iters        = 1000;
-  const double       residual_tolerance = 1e-6;
+  const unsigned int n_max_iters = 1000;
+  const double residual_tolerance = 1e-6;
 
-  unsigned int n_iter        = 0;
-  double       residual_norm = residual_tolerance + 1;
+  unsigned int n_iter = 0;
+  double residual_norm = residual_tolerance + 1;
 
   // We apply the boundary conditions to the initial guess (which is stored in
   // solution_owned and solution).
@@ -250,34 +237,33 @@ HeatNonLinear::solve_newton()
   }
 
   while (n_iter < n_max_iters && residual_norm > residual_tolerance)
+  {
+    assemble_system();
+    residual_norm = residual_vector.l2_norm();
+
+    pcout << "  Newton iteration " << n_iter << "/" << n_max_iters
+          << " - ||r|| = " << std::scientific << std::setprecision(6)
+          << residual_norm << std::flush;
+
+    // We actually solve the system only if the residual is larger than the
+    // tolerance.
+    if (residual_norm > residual_tolerance)
     {
-      assemble_system();
-      residual_norm = residual_vector.l2_norm();
+      solve_linear_system();
 
-      pcout << "  Newton iteration " << n_iter << "/" << n_max_iters
-            << " - ||r|| = " << std::scientific << std::setprecision(6)
-            << residual_norm << std::flush;
-
-      // We actually solve the system only if the residual is larger than the
-      // tolerance.
-      if (residual_norm > residual_tolerance)
-        {
-          solve_linear_system();
-
-          solution_owned += delta_owned;
-          solution = solution_owned;
-        }
-      else
-        {
-          pcout << " < tolerance" << std::endl;
-        }
-
-      ++n_iter;
+      solution_owned += delta_owned;
+      solution = solution_owned;
     }
+    else
+    {
+      pcout << " < tolerance" << std::endl;
+    }
+
+    ++n_iter;
+  }
 }
 
-void
-HeatNonLinear::output(const unsigned int &time_step, const double &time) const
+void HeatNonLinear::output(const unsigned int &time_step, const double &time) const
 {
   DataOut<dim> data_out;
   data_out.add_data_vector(dof_handler, solution, "u");
@@ -296,22 +282,21 @@ HeatNonLinear::output(const unsigned int &time_step, const double &time) const
                      output_file_name;
 
   DataOutBase::DataOutFilter data_filter(
-    DataOutBase::DataOutFilterFlags(/*filter_duplicate_vertices = */ false,
-                                    /*xdmf_hdf5_output = */ true));
+      DataOutBase::DataOutFilterFlags(/*filter_duplicate_vertices = */ false,
+                                      /*xdmf_hdf5_output = */ true));
   data_out.write_filtered_data(data_filter);
   data_out.write_hdf5_parallel(data_filter,
                                output_file_name + ".h5",
                                MPI_COMM_WORLD);
 
   std::vector<XDMFEntry> xdmf_entries({data_out.create_xdmf_entry(
-    data_filter, output_file_name + ".h5", time, MPI_COMM_WORLD)});
+      data_filter, output_file_name + ".h5", time, MPI_COMM_WORLD)});
   data_out.write_xdmf_file(xdmf_entries,
                            output_file_name + ".xdmf",
                            MPI_COMM_WORLD);
 }
 
-void
-HeatNonLinear::solve()
+void HeatNonLinear::solve()
 {
   pcout << "===============================================" << std::endl;
 
@@ -332,22 +317,22 @@ HeatNonLinear::solve()
   unsigned int time_step = 0;
 
   while (time < T - 0.5 * deltat)
-    {
-      time += deltat;
-      ++time_step;
+  {
+    time += deltat;
+    ++time_step;
 
-      // Store the old solution, so that it is available for assembly.
-      solution_old = solution;
+    // Store the old solution, so that it is available for assembly.
+    solution_old = solution;
 
-      pcout << "n = " << std::setw(3) << time_step << ", t = " << std::setw(5)
-            << std::fixed << time << std::endl;
+    pcout << "n = " << std::setw(3) << time_step << ", t = " << std::setw(5)
+          << std::fixed << time << std::endl;
 
-      // At every time step, we invoke Newton's method to solve the non-linear
-      // problem.
-      solve_newton();
+    // At every time step, we invoke Newton's method to solve the non-linear
+    // problem.
+    solve_newton();
 
-      output(time_step, time);
+    output(time_step, time);
 
-      pcout << std::endl;
-    }
+    pcout << std::endl;
+  }
 }
